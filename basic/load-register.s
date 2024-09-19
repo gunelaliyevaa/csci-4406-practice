0dca538bd2589da54813b6b05a1243b0566031ae
Gunel Aliyeva <90043330+gunelaliyevaa@users.noreply.github.com>
2024-09-19 17:05:17 +0400

    Update load-register.s

--- basic/load-register.s
+++ basic/load-register.s
@@ -1,13 +1,15 @@
 .global _start
 _start:
   	mov r3, #0
-	ldr r0, =vector
-	ldr r1, [r0]
-	add r3, r3, r1
+	ldr r0, =vector   //r0 pointer to location of vector, called base register
+	ldr r1, [r0] //load first value of vector to r1
+        ldr r2, [r0, #4] 
+	//#4 means 2nd value of vector which is 4 bytes away.
+	add r3, r3, r1 
 	b done
 
 done:
-	svc #0
+	svc #0 // System call to terminate the program (exit)
 	
 .data
-	vector: .word 0,1,2,3
+	vector: .word 0,1,2,3 // Define the vector (array) with 4 elements: 0, 1, 2, 3
